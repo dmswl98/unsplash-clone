81e25cff32d941841ca65d0e9a8d8ecb3e41d64a
dmswl98 <chej098t@gmail.com>
2022-02-16 23:54:37 +0900

    🔨 Add constant for download image link

--- js/config.js
+++ js/config.js
@@ -1,2 +1,3 @@
-export const URL = `https://api.unsplash.com/photos/random`;
+export const FETCH_URL = `https://api.unsplash.com/photos/random`;
 export const KEY = `R3pRzhHu6cLbhKk5VhcNh3_48_NWI0_qlC1WunVNW2I`;
+export const URL = `https://unsplash.com/photos/`;
