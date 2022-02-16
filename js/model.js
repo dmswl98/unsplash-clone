92ac9a825bf0a1678d6eb52a5de9fb2e1ec905e0
dmswl98 <chej098t@gmail.com>
2022-02-16 23:57:53 +0900

    🔨 Update constant of download image link

--- js/model.js
+++ js/model.js
@@ -1,4 +1,4 @@
-import { URL, KEY } from './config';
+import { FETCH_URL, URL, KEY } from './config';
 export const state = {
   image: [],
 };
@@ -8,7 +8,7 @@ const createImageObject = function (data) {
     return {
       id: img.id,
       imageFile: img.urls.small,
-      downloadLink: img.links.html,
+      downloadLink: `${URL}${img.id}/download?force=true`,
       description: img.description ? img.description.trim() : null,
       userProfileLink: img.user.links.html,
       userName: img.user.name,
@@ -20,7 +20,7 @@ const createImageObject = function (data) {
 
 export const loadImg = async function (count = 10) {
   try {
-    const res = await fetch(`${URL}/?count=${count}&client_id=${KEY}`);
+    const res = await fetch(`${FETCH_URL}/?count=${count}&client_id=${KEY}`);
     const data = await res.json();
     console.log(data);
     state.image = createImageObject(data);
